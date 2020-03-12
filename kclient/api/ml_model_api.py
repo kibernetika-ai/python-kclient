5dc821e9b0f300dd5f2bd16b541bb455724b9772
Nikolay Makhotkin <nikolay.makhotkin@gmail.com>
2020-03-12 11:25:57 -0300

    Fix ml_model_api get version

--- kclient/api/ml_model_api.py
+++ kclient/api/ml_model_api.py
@@ -570,7 +570,7 @@ class MLModelApi(object):
             body=body_params,
             post_params=form_params,
             files=local_var_files,
-            response_type=None,  # noqa: E501
+            response_type='list[ApplicationApplicationModelVersion]',  # noqa: E501
             auth_settings=auth_settings,
             async_req=params.get('async_req'),
             _return_http_data_only=params.get('_return_http_data_only'),
